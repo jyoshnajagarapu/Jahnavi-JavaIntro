510284f5ed81b9996749e0f78f9f84dee5330ce6
Jyothsna Jagarapu <jagarapujyothsna@gmail.com>
2024-05-03 15:31:03 +0530

    test message

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    private static int i,j;
+
+    public static void main(String[] args) {
+        i = 10;
+        j = 5;
+        System.out.printf("%d*%d=%d", j,i,j*i).println();
+    }
+}
